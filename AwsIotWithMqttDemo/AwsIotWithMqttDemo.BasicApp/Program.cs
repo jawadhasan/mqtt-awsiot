103f14433a12cbee72f3efc7e02336b9d7f3944c
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-11-27 14:57:21 +0100

    subscription part added

--- AwsIotWithMqttDemo/AwsIotWithMqttDemo.BasicApp/Program.cs
+++ AwsIotWithMqttDemo/AwsIotWithMqttDemo.BasicApp/Program.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace AwsIotWithMqttDemo.BasicApp
 {
@@ -14,7 +15,7 @@ namespace AwsIotWithMqttDemo.BasicApp
             var broker = "a2upl3efg7dvex-ats.iot.eu-central-1.amazonaws.com"; //<AWS-IoT-Endpoint>           
             var port = 8883;
             var clientId = "TestDeviceWeb";
-            var certPass = "<EnterPassword>";
+            var certPass = "<enterPassword>";
 
             //certificates Path
             var certificatesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "certs");
@@ -28,6 +29,10 @@ namespace AwsIotWithMqttDemo.BasicApp
             // Create a new MQTT client.
             var client = new MqttClient(broker, port, true, caCert, deviceCert, MqttSslProtocols.TLSv1_2);
 
+            //Event wiring
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            client.MqttMsgSubscribed += Client_MqttMsgSubscribed;
+
             //Connect
             client.Connect(clientId);
             Console.WriteLine($"Connected to AWS IoT with client id: {clientId}.");
@@ -35,17 +40,35 @@ namespace AwsIotWithMqttDemo.BasicApp
             //send Messages
             var message = "Hello from .NET Core";
             int i = 1;
-            while (i <= 10)
+            while (i <= 3)
             {
                 client.Publish("topic_2", Encoding.UTF8.GetBytes($"{message} {i}"));
                 Console.WriteLine($"Published: {message} {i}");
                 i++;
                 Thread.Sleep(2000);
             }
+            Console.WriteLine($"Messages Sent Complete.");
 
-            Console.WriteLine($"Messages Sent Complete. Press any key to exit!");
+            //subscribig to topic
+            string topic = "topic_1";
+            client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+
+
+
+            Console.WriteLine($"Press any key to exit!");
             Console.ReadLine();
 
+        }
+
+        private static void Client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+        {
+            Console.WriteLine($"Successfully subscribed to the AWS IoT topic.");
+        }
+
+        private static void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            Console.WriteLine("Message received: " + Encoding.UTF8.GetString(e.Message));
         }
     }
 }
